9ea01efd345697281937eb2a9a5dd02cf48c8f63
Jardel <jardelufc.br>
2018-03-05 11:05:07 -0300

    Jesus is the lord

--- src/test/scala/mult8x8/Mult8x8Tests.scala
+++ src/test/scala/mult8x8/Mult8x8Tests.scala
@@ -5,9 +5,9 @@ import chisel3.iotesters.PeekPokeTester
 
 class Mult8x8Tests(c: Mult8x8) extends PeekPokeTester(c) {
   val maxInt  = 0xFF
-  //for (i <- 0 until 10) {
-    val x = 0xab //rnd.nextInt(maxInt)
-    val y = 0xcd       //rnd.nextInt(maxInt)
+  for (i <- 0 until 10) {
+    val x = rnd.nextInt(maxInt)
+    val y = rnd.nextInt(maxInt)
     poke(c.io.a, x)
     poke(c.io.b, y)
     poke(c.io.start,0)
@@ -15,7 +15,7 @@ class Mult8x8Tests(c: Mult8x8) extends PeekPokeTester(c) {
     step(1)
     poke(c.io.start,0)
     poke(c.io.reset,0)
-    step(4)
+    step(1)
     poke(c.io.start,1)
     step(1)
     poke(c.io.start,0)
@@ -29,6 +29,7 @@ class Mult8x8Tests(c: Mult8x8) extends PeekPokeTester(c) {
     expect(c.io.done_flag, 1)
     step(1)
     expect(c.io.result, x * y)
+    expect(c.io.done_flag, 0)
     //step(1)
-  //}
+  }
 }
